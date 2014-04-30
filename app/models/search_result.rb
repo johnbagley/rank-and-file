d1eed3ac48c38e1caf8cd0a5453ec5f93568f07b
John Bagley <jdbagley@gmail.com>
2014-04-30 13:30:29 -0400

    remove search_result model

--- app/models/search_result.rb
+++
@@ -1,6 +0,0 @@
-class SearchResult
-  include HTTParty
-  format :html
-
-  HTTParty.get('https://congress.api.sunlightfoundation.com/legislators?last_name=Warren&apikey=acfdba04c94a4714b2c80d6c6a40329f')
-end
