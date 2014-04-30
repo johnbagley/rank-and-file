4f1bb633d7d5df6598653af680df85e2040ff2b5
John Bagley <jdbagley@gmail.com>
2014-04-30 13:31:54 -0400

    fix pull request issue

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,8 @@
 class MembersController < ApplicationController
 
   def create
-    @member = Member.find_or_create_by(member_params)
-    redirect_to search_result_path(@member)
+    member = Member.find_or_create_by(member_params)
+    redirect_to search_result_path(member)
   end
 
   private
