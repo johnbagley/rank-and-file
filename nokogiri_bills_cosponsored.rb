a2c34a02b43a569e01a82854bf4b2482bb77b7ac
John Bagley <jdbagley@gmail.com>
2014-05-08 17:02:51 -0400

    add other nokogiri files

---
+++ nokogiri_bills_cosponsored.rb
@@ -0,0 +1,25 @@
+#scrapes congress.gov for the number of bills cosponsored by a member, taking in their first and last name through string interpolation
+
+require 'open-uri'
+require 'nokogiri'
+
+first_name = 'bobby'
+last_name = 'jindal'
+
+puts 'Hello World'
+
+url = "http://beta.congress.gov/member?q=#{first_name}-#{last_name}"
+doc = Nokogiri::HTML(open(url))
+member_with_member_number = doc.css('ul.results_list li h2 a')
+top_result = member_with_member_number[0].to_s
+words = top_result.split('"')
+parsed_words = words[1].split('?')
+parsed_member_path = parsed_words[0]
+puts parsed_member_path
+
+parsed_url = "#{parsed_member_path}?q=%7B%22search%22%3A%5B%22bobby+jindal%22%5D%2C%22type%22%3A%22bills%22%2C%22sponsorship%22%3A%22Cosponsored+Legislation%22%7D"
+parsed_doc = Nokogiri::HTML(open(parsed_url))
+wombat = parsed_doc.css('#searchTune span').text
+words = wombat.split(/\W+/)
+number_of_bills = words.last
+puts "Number of bills: #{number_of_bills}"
