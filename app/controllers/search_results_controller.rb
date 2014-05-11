045b0d689f47cc59047de8a11fd8fcb08d87a163
John Bagley <jdbagley@gmail.com>
2014-05-11 18:12:02 -0400

    fix pr issue

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -2,14 +2,31 @@ class SearchResultsController < ApplicationController
 
   def show
     @member = Member.find_by_name(params[:query][:name])
-    @bills_sponsored = BillsSponsored.new(
-      @member.first_name, @member.last_name).number_of_bills
-    @bills_cosponsored = BillsCosponsored.new(
-      @member.first_name, @member.last_name).number_of_cosponsored_bills
-    @amendments_sponsored = AmendmentsSponsored.new(
-      @member.first_name, @member.last_name).number_of_amendments_sponsored
-    @amendments_cosponsored = AmendmentsCosponsored.new(
-      @member.first_name, @member.last_name).number_of_amendments_cosponsored
+    @bills_sponsored = bills_sponsored
+    @bills_cosponsored = bills_cosponsored
+    @amendments_sponsored = amendments_sponsored
+    @amendments_cosponsored = amendments_cosponsored
+  end
+
+  private
+
+  def bills_sponsored
+    BillsSponsored.new(@member.first_name, @member.last_name).number_of_bills
+  end
+
+  def bills_cosponsored
+    BillsCosponsored.new(@member.first_name, @member.last_name).
+      number_of_cosponsored_bills
+  end
+
+  def amendments_sponsored
+    AmendmentsSponsored.new(@member.first_name, @member.last_name).
+      number_of_amendments_sponsored
+  end
+
+  def amendments_cosponsored
+    AmendmentsCosponsored.new(@member.first_name, @member.last_name).
+      number_of_amendments_cosponsored
   end
 
 end
