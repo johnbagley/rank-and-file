29793ae0c09d8deb95f05975e6fcf9cc64007bc8
John Bagley <jdbagley@gmail.com>
2014-07-26 02:03:06 -0400

    add name scraper

--- app/models/senator_url.rb
+++ app/models/senator_url.rb
@@ -3,10 +3,23 @@ require 'nokogiri'
 class SenatorURL
 
   def build_senator_url
-    scrape
+    scrape_url
+    scrape_name
   end
 
-  def scrape
+  def scrape_name
+    names = []
+    doc = Nokogiri::HTML(open('https://beta.congress.gov/members'))
+    scraped = doc.css('#members-senators option').each do |name|
+      strings = name.to_s.split('>')
+      strings = strings[1].split('[')
+      names << strings[0]
+    end
+    puts names
+
+  end
+
+  def scrape_url
     links = []
     doc = Nokogiri::HTML(open('https://beta.congress.gov/members'))
     scraped = doc.css('#members-senators option').each do |link|
